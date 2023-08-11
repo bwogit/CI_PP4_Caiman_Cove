f54e3d3ba2925563def636cfec8ad1bc83320fa0
bwogit <jeremie.sandot@hotmail.com>
2023-08-11 12:34:57 +0000

    Add cloudinary settinmgs to env, settins.py amd heroky variable

--- caiman_cove/settings.py
+++ caiman_cove/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-bwogit-cipp4caimancove-sq9vicfqokt.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['8000-bwogit-cipp4caimancove-sq9vicfqokt.ws-eu102.gitpod.io', 'caiman_cove.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'food_and_drinks',
 ]
 
@@ -61,7 +63,7 @@ ROOT_URLCONF = 'caiman_cove.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
