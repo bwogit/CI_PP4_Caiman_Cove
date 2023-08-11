f4481ba1359f831bb057d658f893e86c78988258
bwogit <jeremie.sandot@hotmail.com>
2023-08-11 13:26:49 +0000

    Design menu models and add to models.py

--- food_and_drinks/models.py
+++ food_and_drinks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+FOOD_ITEM_TYPE = ((0, 'Starters'), (1, 'Mains'), (2, 'Desserts'), (3, 'AddItem'))
+DRINK_ITEM_TYPE = ((0, 'Soft'), (1, 'Wine'), (2, 'Beers'), (3, 'AddItem'))
+
+class FoodMenuItem(models.Model):
+
+
+class DrinkMenuItem(models.Model):    
