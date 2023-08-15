955307e42709165f97ba8e29992cd48ecc381ca1
bwogit <jeremie.sandot@hotmail.com>
2023-08-15 22:00:08 +0000

    Add list filter to FoodMenuAdmin and DrinkMenuAdmin

--- food_and_drinks/admin.py
+++ food_and_drinks/admin.py
@@ -6,9 +6,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(FoodMenuItem)
 class FoodMenuAdmin(SummernoteModelAdmin):
+    list_filter = ('available', 'food_type')
     summernote_fields = ('food_description')
 
 
 @admin.register(DrinkMenuItem)
 class DrinkMenuAdmin(SummernoteModelAdmin):
+    list_filter = ('available', 'drink_type')
     summernote_fields = ('drink_description')    
