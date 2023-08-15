c5fb932efa3a7a65d1401850b9b74b0187e6408d
bwogit <jeremie.sandot@hotmail.com>
2023-08-15 23:24:47 +0000

    Add a class for the table model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    """
+    class for the Table model(table)
+    """
+    table_id = models.AutoField(primary_key=True)
+    table_number = models.CharField(
+        max_length=10, default='New Table', unique=True)
+    capacity = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['-capacity']
+
+    def __str__(self):
+        return self.table_name
