a7274726cdde26ddfd581d87d28501d631cf631f
bwogit <jeremie.sandot@hotmail.com>
2023-08-16 15:54:47 +0000

    food_and_drinks/views | creating views.py from models.py and add templates 404, base, index, menu.html

--- food_and_drinks/views.py
+++ food_and_drinks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import FoodMenuItem, DrinkMenuItem
 
-# Create your views here.
+class FoodMenuListView(ListView):
+    """
+    Class-based view to display the food menu items.
+    """
+    model = FoodMenuItem
+    template_name = 'food_menu.html'
+    context_object_name = 'food_items'
+    queryset = FoodMenuItem.objects.filter(food_available=True)
+
+class DrinkMenuListView(ListView):
+    """
+    Class-based view to display the drink menu items.
+    """
+    model = DrinkMenuItem
+    template_name = 'drink_menu.html'
+    context_object_name = 'drink_items'
+    queryset = DrinkMenuItem.objects.filter(drink_available=True)
