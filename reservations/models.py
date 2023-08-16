6b947b1548a767c49f4e5ed536d9bf504ac4ccc6
bwogit <jeremie.sandot@hotmail.com>
2023-08-16 09:21:36 +0000

    reservations/model.py | creating Table reservation

--- reservations/models.py
+++ reservations/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
 class Table(models.Model):
     """
-    class for the Table model(table)
+    A class for the Table model(table)
     """
     table_id = models.AutoField(primary_key=True)
     table_number = models.CharField(
@@ -16,3 +17,27 @@ class Table(models.Model):
     def __str__(self):
         return self.table_number
 
+class Reservation(models.Model):
+    """
+    A class for the reservation model
+    """
+    reservation_id = models.AutoField(primary_key=True)
+    reservation_time = models.DateTimeField(auto_now_add=True)
+    reserved_date = models.DateField()
+    reserved_time_slot = models.CharField(
+        max_length=25, choices=custom_time_slots, default='12:00')
+    reserved_table = models.ForeignKey(
+        Table, on_delete=models.CASCADE, related_name="table_reserved",
+        null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user", null=True)
+    reservation_status = models.CharField(max_length=25, unique=True)
+
+    class Meta:
+        ordering = ['-reserved_time_slot']
+
+    def __str__(self):
+        return str(self.reservation_id)
+
+
+class User(models.Model):
