b42f89742a99470afbd2016fbd9261cf6373f15f
bwogit <jeremie.sandot@hotmail.com>
2023-08-17 21:09:41 +0000

    home page is displayed at index

--- caiman_cove/settings.py
+++ caiman_cove/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'food_and_drinks',
     'reservations',
-    'home', 
+    'home',
 ]
 
 MIDDLEWARE = [
--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 
-def home(request):
+def index(request):
     """
     a view to display the homepage
     """
