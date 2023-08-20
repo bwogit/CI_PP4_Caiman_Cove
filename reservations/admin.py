b4650f8485f6b5e0f163ea07078ccda4c75415cf
bwogit <jeremie.sandot@hotmail.com>
2023-08-20 11:30:50 +0000

    reservation/admin.py | add all feild from Rservation table to admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -10,8 +10,8 @@ class TableAdmin(admin.ModelAdmin):
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('reservation_id', 'reserved_date', 'reserved_time_slot', 'reserved_table', 'customer', 'reservation_status')
-    list_filter = ('reserved_date', 'reserved_time_slot', 'reservation_status')
+    list_display = ('reservation_id','reservation_time', 'reserved_date', 'reserved_time_slot', 'reserved_table', 'customer', 'reservation_status','customer_count')
+    list_filter = ('reservation_id','reservation_time', 'reserved_date', 'reserved_time_slot', 'reserved_table', 'customer', 'reservation_status','customer_count')
     search_fields = ('reserved_table__table_number', 'customer__customer_name')
 
 @admin.register(Customer)
