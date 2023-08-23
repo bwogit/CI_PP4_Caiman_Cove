236bfbdd49037254b34896f1f4010f8e304960fc
bwogit <jeremie.sandot@hotmail.com>
2023-08-23 15:34:17 +0000

    reservations/admin.py | add correct indentations to fucntion approve_reservation

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from .models import Table, Reservation
 
-# Define the custom admin action function
-def approve_reservations(modeladmin, request, queryset):
-    queryset.update(reservation_status='confirmed')
-approve_reservations.short_description = "Approve selected reservations"
+
 
 # Register your models here.
 @admin.register(Table)
@@ -13,13 +10,8 @@ class TableAdmin(admin.ModelAdmin):
     list_filter = ('capacity',)
     search_fields = ('table_number',)
 
-# @admin.register(Reservation)
-# class ReservationAdmin(admin.ModelAdmin):
-#     list_display = ('reservation_id','reservation_time', 'reserved_date', 'reserved_time_slot', 'customer', 'reservation_status','customer_count')
-#     list_filter = ('reservation_status',)
-#     actions = ['approve_reservations']
-#     search_fields = ('reserved_table__table_number', 'customer__customer_name')
-# Registration of bookings to display in the admin panel
+# Define the custom admin action function
+
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
@@ -47,15 +39,12 @@ class ReservationAdmin(admin.ModelAdmin):
         'reserved_time_slot',
         'reservation_time',)
     
-    actions = ['confirm_reservation']
+    search_fields = ['name']
+    actions = ['approve_reservation']
 
-    def confirm_reservations(self, request, queryset):
-        queryset.update(status='Reservation Confirmed')
+    def approve_reservation(modeladmin, request, queryset):
+        queryset.update(status='confirmed')
 
 
-# @admin.register(Customer)
-# class CustomerAdmin(admin.ModelAdmin):
-#     list_display = ('customer_id', 'customer_name', 'phone', 'email', 'created_date')
-#     search_fields = ('customer_name', 'email')
 
 
