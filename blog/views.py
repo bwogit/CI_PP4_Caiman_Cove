828648bbde7e743c480c2502fb1662248e45867f
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 13:57:11 +0000

    blog/views.py | Create class-based views for listing blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+class BlogListView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'blog/blog_list.html', {'posts': posts})
