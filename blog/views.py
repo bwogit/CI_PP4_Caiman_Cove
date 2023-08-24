a55695ccffd6438629d5e2954d02447b611890f2
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 20:33:02 +0000

    blog/views.py | add status 1 whic will concern all published

--- blog/views.py
+++ blog/views.py
@@ -7,20 +7,20 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 class BlogList(View):
     def get(self, request):
-        posts = Post.objects.all()
+        posts = Post.objects.filter(status=1)
         return render(request, 'blog/blog_list.html', {'posts': posts})
 
 class BlogDetail(View):
     # A class to display the details of the blog #
     
     def get(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = get_object_or_404(Post, pk=pk, status=1)
         return render(request, 'blog/blog_detail.html', {'post': post})    
 
 class AddComment(View):
     @login_required
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = get_object_or_404(Post, pk=pk, status=1)
         name = request.user.username
         email = request.user.email
         body = request.POST.get('comment_body')
