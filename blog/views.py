2c3ba206bf062cc016e4b5f93489ff47ed126d58
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 15:10:36 +0000

    blog/views.py | change to class based approach

--- blog/views.py
+++ blog/views.py
@@ -7,16 +7,31 @@ from django.contrib.auth.decorators import login_required
 class BlogListView(View):
     def get(self, request):
         posts = Post.objects.all()
-        return render(request, 'blog/blog_list.html', {'posts': posts})
+        return render(request, 'blog/posts_list.html', {'posts': posts})
 
+    # @login_required
+    # def add_comment_to_post(request, pk):
+    # post = get_object_or_404(Post, pk=pk)
+    # if request.method == 'POST':
+    #     name = request.user.username
+    #     email = request.user.email
+    #     body = request.POST['comment_body']
+    #     comment = Comment(post=post, name=name, email=email, body=body)
+    #     comment.save()
+    #     return redirect('blog_detail', pk=post.pk)
+    # return render(request, 'blog/comments.html', {'post': post})
+
+    class AddCommentView(View):
     @login_required
-    def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'POST':
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         name = request.user.username
         email = request.user.email
-        body = request.POST['comment_body']
+        body = request.POST.get('comment_body')
         comment = Comment(post=post, name=name, email=email, body=body)
         comment.save()
-        return redirect('blog_detail', pk=post.pk)
-    return render(request, 'blog/add_comment_to_post.html', {'post': post})
+        return redirect('posts_list', pk=post.pk)
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/comments.html', {'post': post})
