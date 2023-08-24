1ce59cfbb26bb145bf91cd69d96e24f5a7a7d331
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 19:39:07 +0000

    blog/views.py | add BlogDetails to dipslay the detail of a single blog

--- blog/views.py
+++ blog/views.py
@@ -10,18 +10,10 @@ class BlogList(View):
         posts = Post.objects.all()
         return render(request, 'blog/blog_list.html', {'posts': posts})
 
-    # @login_required
-    # def add_comment_to_post(request, pk):
-    # post = get_object_or_404(Post, pk=pk)
-    # if request.method == 'POST':
-    #     name = request.user.username
-    #     email = request.user.email
-    #     body = request.POST['comment_body']
-    #     comment = Comment(post=post, name=name, email=email, body=body)
-    #     comment.save()
-    #     return redirect('blog_detail', pk=post.pk)
-    # return render(request, 'blog/comments.html', {'post': post})
-
+class BlogDetail(View):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/blog_detail.html', {'post': post})    
 
 class AddComment(View):
     @login_required
