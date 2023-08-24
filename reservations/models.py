4a88aefde9824770ed4fae4654414a03c25fc9a8
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 12:02:56 +0000

    improving user readalbility in models.py

--- reservations/models.py
+++ reservations/models.py
@@ -10,10 +10,10 @@ from django.contrib.auth.models import User
 custom_time_slots = (
     ('12:00', '12:00 PM'),
     ('13:00', '13:00 PM'),
-    ('17:00', '08:00 PM'),
-    ('18:00', '22:00 PM'),
-    ('19:00', '20:00 PM'),
-    ('21:00', '21:00 PM'),
+    ('17:00', '17:00 PM'),
+    ('18:00', '18:00 PM'),
+    ('19:00', '19:00 PM'),
+    ('20:00', '21:00 PM'),
     ('22:00', '22:00 PM'),
     ('23:00', '23:00 PM'),
    
@@ -42,57 +42,6 @@ class Table(models.Model):
 
     def __str__(self):
         return self.table_number
-
-
-#class Customer(models.Model):
-#    """
-#    a class for the Customer model
-#    """
-#    customer_id = models.AutoField(primary_key=True)
-#    created_date = models.DateTimeField(auto_now_add=True)
-#    customer_name = models.CharField(max_length=80)
-#    phone = PhoneNumberField()
-#    email = models.EmailField(max_length=254, default="example@example.com", validators=[EmailValidator()])
-#    
-#    class Meta:
-#        ordering = ['-created_date']
-#    def __str__(self):
-#        return self.customer_name # Return a meaningful representation, like the customer's name#
-#
-
-# class Reservation(models.Model):
-#     """
-#     A class for the reservation model
-#     """
-#     def clean(self):
-#         if self.reserved_date < timezone.now().date():
-#             raise ValidationError("Reserved date must be in the future.")
-
-#     reservation_id = models.AutoField(primary_key=True)
-#     reservation_time = models.DateTimeField(auto_now_add=True)
-#     reserved_date = models.DateField()
-#     reserved_time_slot = models.CharField(
-#         max_length=25, choices=custom_time_slots, default='12:00')
-#     reserved_table = models.ForeignKey(
-#         Table, on_delete=models.CASCADE, related_name="table_reserved",
-#         null=True)
-#     customer = models.ForeignKey(
-#         Customer, on_delete=models.CASCADE, related_name="customer", null=True)
-#     reservation_status = models.CharField(max_length=35, choices=status_options, default='awaiting confirmation')
-#     customer_capacity = (
-#         (2, "2 Customers"),
-#         (3, "3 Customers"),
-#         (4, "4 Customers"),
-#         (5, "5 Customers"),
-#         (6, "6 Customers"),
-#         )
-#     customer_count = models.PositiveIntegerField(choices=customer_capacity, default=2) 
-
-#     class Meta:
-#         ordering = ['-reserved_time_slot']
-
-#     def __str__(self):
-#         return str(self.reservation_id)
 
 class Reservation(models.Model):
     """
