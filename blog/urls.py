c669e5421fca284c88da8d1d11aa9f36ebdb3d45
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 19:48:57 +0000

    blog/urls.py | add blog_detail.html to urls.py coorect typo

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import BlogList, AddComment
 urlpatterns = [
     path('blog_list/', views.BlogList.as_view(), name='blog_list'),
     path('add_comment/<int:pk>/', views.AddComment.as_view(), name='add_comment'),
-    path('blog_detail', views.BlogDetails.as_view(), name='add_comment')
+    path('blog_detail', views.BlogDetail.as_view(), name='add_comment')
     
 ]
 
