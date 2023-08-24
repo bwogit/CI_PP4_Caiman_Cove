f261dd4f21645833beecd97154ea7287a9a2cf58
bwogit <jeremie.sandot@hotmail.com>
2023-08-24 15:16:24 +0000

    blog/urls.py | add urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from .views import BlogList, AddComment
+
+urlpatterns = [
+    path('blog/blog_list/', BlogList.as_view(), name='blog_list'),
+    path('blog/<int:pk>/add_comment/', AddComment.as_view(), name='add_comment'),
+    
+]
