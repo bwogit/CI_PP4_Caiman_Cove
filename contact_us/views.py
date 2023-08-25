7cc3cf4c7a6c671d78607555c939d8dadbf9fa68
bwogit <jeremie.sandot@hotmail.com>
2023-08-25 08:57:06 +0000

    contact_us/views.py | add LoginRequiredMixin

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ContactForm
 
-class ContactUser(View):
+class ContactUserView(LoginRequiredMixin, View):
     template_name = 'contact_us.html'
 
     def get(self, request, *args, **kwargs):
         # Check if the user is logged in
-        if request.user.is_authenticated:
-            initial_data = {'email': request.user.email}
-        else:
-            initial_data = {}
+        # if request.user.is_authenticated:
+        #     initial_data = {'email': request.user.email}
+        # else:
+        #     initial_data = {}
+        user = request.user
+        contact_form = ContactForm(initial={'email': user.email, 'name': user.username} if user.is_authenticated else {})
+        return render(request, self.template_name, {'contact_form': contact_form})
 
         contact_form = ContactForm(initial=initial_data)
         return render(request, self.template_name, {'contact_form': contact_form})
