00301ad6ba332b83bc769c5ae55ae76c4613109e
bwogit <jeremie.sandot@hotmail.com>
2023-08-25 08:48:04 +0000

    move contact_us.html one up

--- contact_us/urls.py
+++ contact_us/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ContactUser
 
 urlpatterns = [
-    path('contact_us', ContactUser.as_view(), name='contact_us'),
+    path('contact_us/', ContactUser.as_view(), name='contact_us'),
     
 ]
