7fe81b7fe2370e9dc3660cc08cb2d9071286b1f2
bwogit <jeremie.sandot@hotmail.com>
2023-08-25 00:38:26 +0000

    Create model and form

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class Contact(models.Model):
+    message_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="contact_user",
+        null=True
+    )
+    name = models.CharField(max_length=50, null=True)
+    email = models.EmailField(max_length=254, default="")
+    phone = PhoneNumberField(null=True)
+    message = models.TextField()
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.name
