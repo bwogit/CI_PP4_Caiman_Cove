33b1d0881a0ec4f4218627daf7d39cea793e7447
bwogit <jeremie.sandot@hotmail.com>
2023-08-25 23:14:11 +0000

    adjust the pagination settings and loop through the posts in groups of 2

--- blog/models.py
+++ blog/models.py
@@ -30,8 +30,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def number_of_likes(self):
-        return self.likes.count()
+    # def number_of_likes(self):
+    #     return self.likes.count()
 
 
 class Comment(models.Model):
