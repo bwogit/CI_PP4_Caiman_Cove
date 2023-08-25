e212f3ba44fba8b0aac3383e5d61e0fe7a91e15e
bwogit <jeremie.sandot@hotmail.com>
2023-08-25 08:18:28 +0000

    contact_us/views.py | create contactUser view

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import ContactForm
 
-# Create your views here.
+class ContactUserView(View):
+    template_name = 'contact_user.html'
+
+    def get(self, request, *args, **kwargs):
+        # Check if the user is logged in
+        if request.user.is_authenticated:
+            initial_data = {'email': request.user.email}
+        else:
+            initial_data = {}
+
+        contact_form = ContactForm(initial=initial_data)
+        return render(request, self.template_name, {'contact_form': contact_form})
