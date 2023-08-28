1414c84a5da27a6a712302b1b4ef31889b71a130
bwogit <jeremie.sandot@hotmail.com>
2023-08-28 18:14:53 +0000

    Create def custom_404_view

--- home/views.py
+++ home/views.py
@@ -6,3 +6,7 @@ def home(request):
     a view to display the homepage
     """
     return render(request, 'home/index.html')
+
+
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
