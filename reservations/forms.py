6a7c637cede5f8ad7f3f26c5110797483f8d483b
bwogit <jeremie.sandot@hotmail.com>
2023-08-28 17:00:46 +0000

    reservations/forms.py | pep8 compliant

--- reservations/forms.py
+++ reservations/forms.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from phonenumber_field.formfields import PhoneNumberField
 from .models import Reservation
 
+
 class BookingForm(forms.ModelForm):
     """
     A form to gather booking information from users.
@@ -12,9 +13,10 @@ class BookingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        
+
     reserved_date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date', 'min': datetime.now().date()}))
+        widget=forms.DateInput(attrs={'type': 'date', 'min':
+                               datetime.now().date()}))
 
     phone = PhoneNumberField(widget=forms.TextInput(
         attrs={'placeholder': ('+353123456789')}))
