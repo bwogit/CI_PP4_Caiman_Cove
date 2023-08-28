24f0a8f724256f90326a32599b1f9c5f7ff21b96
bwogit <jeremie.sandot@hotmail.com>
2023-08-28 18:33:18 +0000

    Create def custom_404_view add bootstrap styling

--- home/views.py
+++ home/views.py
@@ -8,5 +8,5 @@ def home(request):
     return render(request, 'home/index.html')
 
 
-def custom_404_view(request, exception):
-    return render(request, '404.html', status=404)
+# def custom_404_view(request, exception):
+#     return render(request, '404.html', status=404)
