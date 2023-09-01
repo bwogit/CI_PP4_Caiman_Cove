0470ccaaac81dcf4a62b60ad4ee3174864882702
bwogit <jeremie.sandot@hotmail.com>
2023-09-01 09:32:33 +0000

    removed all modifications pertaining to change password

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('change_password/', views.change_password, name='change_password'),
 ]
--- home/views.py
+++ home/views.py
@@ -10,21 +10,3 @@ def home(request):
     a view to display the homepage
     """
     return render(request, 'home/index.html')
-
-
-@login_required
-def change_password(request):
-    """
-    A view for changing the password, accessible only for logged-in users
-    taken from Django 
-    """
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            messages.success(request,
-                             'Your password was successfully updated!')
-            return redirect('profile')  # Redirect to the user's profile page
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'change_password.html', {'form': form})
